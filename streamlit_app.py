12bd028079534f773711144a535d0b55afc34743
Karri Linnoinen <karri.linnonen@gmail.com>
2023-02-05 13:20:34 +0200

    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
-my_conn = snowflake.connector.connect(**streamlit.secret["snowflake"])
+my_conn = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_conn.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
