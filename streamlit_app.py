6ccf3cf033f96202f7a483bf2cffb4aaf165d79c
Karri Linnoinen <karri.linnonen@gmail.com>
2023-02-05 12:31:03 +0200

    user input added

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,9 @@ streamlit.dataframe(fruits_to_show)
 
 # get api response
 streamlit.header('Fruityvice Fruit Advise!')
+fruit_choice = streamlit.text_input('What fruit would you like info about?, 'Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #streamlit.text(fruityvice_response.json())
 
