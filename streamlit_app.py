51a656f26cc5e706996a8425cebec21d52a45ba5
Karri Linnoinen <karri.linnonen@gmail.com>
2023-02-05 12:32:33 +0200

    user input added to api get

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.header('Fruityvice Fruit Advise!')
 fruit_choice = streamlit.text_input('What fruit would you like info about?', 'Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 # normalize json response and display
