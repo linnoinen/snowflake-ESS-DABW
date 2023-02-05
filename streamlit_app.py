e3a25052f039ef513a1f1ab1c10c293248e36df9
Karri Linnoinen <karri.linnonen@gmail.com>
2023-02-05 12:26:26 +0200

    header to api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,5 +22,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # display fruits
 streamlit.dataframe(fruits_to_show)
 
+# display api response
+streamlit.header('Fruityvice Fruit Advise!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
